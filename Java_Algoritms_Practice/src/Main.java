3f9775d339cd1e1060cec35486838cbe37b7a6b8
PresF1 <vadimgolacov@gmail.com>
2023-04-04 16:28:12 +0200

    :sparkles: - get max integer algorithm completed

--- Java_Algoritms_Practice/src/Main.java
+++ Java_Algoritms_Practice/src/Main.java
@@ -1,4 +1,4 @@
-import tasks.Strings;
+import tasks.MathOperations;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -7,15 +7,16 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String input, output;
+        int num1, num2;
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Type your string here: ");
-        input = reader.readLine();
+        System.out.println("Enter the first number: ");
+        num1 = Integer.parseInt(reader.readLine());
 
-        output = Strings.reverse(input);
+        System.out.println("Enter the second number: ");
+        num2 = Integer.parseInt(reader.readLine());
 
-        System.out.println("Here is your reversed string: ");
-        System.out.println(output);
+        System.out.println("Here is the max number: ");
+        System.out.println(MathOperations.GetMax(num1, num2));
     }
 }
---
+++ Java_Algoritms_Practice/src/tasks/MathOperations.java
@@ -0,0 +1,10 @@
+package tasks;
+
+// создать метод который принимает на вход 2 числа и возвращает то которое больше
+// в случае равенства возвращать любое и выводить сообщение о равенстве
+public class MathOperations {
+
+    public static int GetMax(int num1, int num2) {
+        return num1 >= num2 ? num1 : num2;
+    }
+}
